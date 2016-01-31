053ec55bb48266c523a4add0de7892f1213e9e8c
Magnus Thor <magnus@xsockets.net>
2016-01-31 16:52:57 +0100

    First commit

---
+++ src/XSockets.WebRC.Video.Recorder.beta.js
@@ -0,0 +1,51 @@
+﻿var XSockets = XSockets || {};
+var sourceBuffer,mediaSource = new MediaSource();
+mediaSource.addEventListener('sourceopen', handleSourceOpen, false);
+function handleSourceOpen(event) {
+   
+    sourceBuffer = mediaSource.addSourceBuffer('video/webm; codecs="vp8"');
+   
+}
+XSockets.MediaRecorder = (function () {
+    var recorder = function (stream, options) {
+        var self = this;
+        this.options = options || { mimeType: "video/webm", ignoreMutedMedia: false };
+        var mediaRecorder = new MediaRecorder(stream, this.options);
+        var handleStop = function (event) {
+            self.isRecording = false;
+            var blob = new Blob(self.blobs, { type: self.options.mimeType });
+            self.oncompleted.apply(self, [blob, URL.createObjectURL(blob)]);
+        };
+        var handleDataAvailable = function (event) {
+            if (event.data && event.data.size > 0)
+                self.blobs.push(event.data);
+        };
+        mediaRecorder.onstop = handleStop;
+        mediaRecorder.ondataavailable = handleDataAvailable;
+        this.mediaRecorder = mediaRecorder;
+    };
+    recorder.prototype.oncompleted = function() {
+    };
+
+    recorder.prototype.stop = function()
+    {
+        var mediaRecorder = this.mediaRecorder;
+        mediaRecorder.stop();
+    }
+    recorder.prototype.blobs = [];
+    recorder.prototype.start = function (ms, stop) {
+        this.blobs.length = 0;
+        this.isRecording = true;
+        var mediaRecorder = this.mediaRecorder;
+        var timeOut = window.setTimeout(function () {
+            console.log("max time to record exceeded");
+            mediaRecorder.stop();
+            window.clearTimeout(timeOut);
+        }, stop || 3001);
+        mediaRecorder.start(ms);
+    };
+    recorder.prototype.isRecording = false;
+
+    return recorder;
+
+})();
